106ff8330a261824e7d3cfcf3db189ace4d4d50c
sahrais2cool <sahravann0@gmail.com>
2023-07-14 10:31:12 -0700

    Initial Commit

---
+++ questions/questionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  questionsApp.swift
+//  questions
+//
+//  Created by Scholar on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct questionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
